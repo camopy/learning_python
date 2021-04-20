b7de9bffe4d7bec46d74cb5ddb3273e0a32860ba
Paulo Camopy <paulorenato.cpb@gmail.com>
2021-04-20 16:47:57 -0300

    added currency conversion

--- string_manipulation/main.py
+++ string_manipulation/main.py
@@ -8,5 +8,17 @@ url_params = url_extractor.get_url_params()
 
 print(url_base)
 print(url_params)
+
 print(url_extractor.get_param_value("moedaOrigem"))
 # print(url_extractor.get_param_value("testerror"))
+
+print(url_extractor)
+
+DOLLAR_VALUE = 5.50
+
+from_currency = url_extractor.get_param_value("moedaOrigem")
+to_currency = url_extractor.get_param_value("moedaDestino")
+quantity = url_extractor.get_param_value("quantidade")
+
+
+print(int(quantity) * DOLLAR_VALUE)
