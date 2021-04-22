49b0836cc56da160921e1f6d738b9b2bee33170f
Paulo Camopy <paulorenato.cpb@gmail.com>
2021-04-22 14:06:11 -0300

    feat: :sparkles: learning timedelta

--- oo/dates.py
+++ oo/dates.py
@@ -2,12 +2,23 @@ from datetime import datetime, timedelta
 
 
 class Date:
-    def __init__(self):
-        self.date = datetime.today()
+    def __init__(self, date):
+        self.date = date
 
     def format(self):
         return self.date.strftime("%d/%m/%Y %H:%M")
 
+    def time_from_date(self, date):
+        return date - self.date
 
-date = Date()
-print(date.format())
+    def __str__(self):
+        return self.format()
+
+
+today = Date(datetime.today())
+one_week_later = datetime.today() + timedelta(days=7)
+
+print("Now:", today)
+print("1 week later:", Date(one_week_later))
+
+print("Time between:", today.time_from_date(one_week_later))
