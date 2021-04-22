1b95ba3b41b52d41300d6ed125dad6ed0a1ad2cf
Paulo Camopy <paulorenato.cpb@gmail.com>
2021-04-22 13:37:29 -0300

    refactor: :recycle: refactored format method name

--- oo/dates.py
+++ oo/dates.py
@@ -4,9 +4,9 @@ class Date:
         self.day = day
         self.year = year
 
-    def formated(self):
+    def format(self):
         return f"{self.month}/{self.day}/{self.year}"
 
 
 date = Date(4, 18, 2021)
-print(date.formated())
+print(date.format())
